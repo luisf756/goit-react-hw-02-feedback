b304074a835245c5510a7d74dfc93d9a9d0a806c
luisf756 <luchof756@gmail.com>
2023-06-06 02:07:44 -0500

    change warng in statistics

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,28 +1,31 @@
 // import style from './Satistics.module.css';
 import React from 'react';
 
-export const Statistics = ({ state, total, posFid }) => {
+export const Statistics = ({ state, total, posFid }) => {  
     
-    {
-      if (total >= 1) {
+    if(total>=1){
         return (
-          <>
-            <h2>Statistics</h2>
-            <p>good: {state.good}</p>
-            <p>Neutral: {state.neutral}</p>
-            <p>Bad: {state.bad}</p>
-            <p>Total: {total}</p>
-            <p>Positive feedback: {posFid} %</p>
-          </>
-        );
-      } else {
-        return (
-          <>
-            <h2>Statistics</h2>
-            {/* <Notification></Notification> */}
-          </>
-        );
-      }
+            <>
+              <h2>Statistics</h2>
+              <p>good: {state.good}</p>
+              <p>Neutral: {state.neutral}</p>
+              <p>Bad: {state.bad}</p>
+              <p>Total: {total}</p>
+              <p>Positive feedback: {posFid} %</p>
+            </>
+          );
     }
-    
+    return (
+        <>
+          <h2>Statistics</h2>
+          <Notification></Notification>
+        </>
+    );
+  };
+
+  
+  const Notification=()=>{
+    return (<>
+        <p>No feedback given</p>
+        </>);
   };
